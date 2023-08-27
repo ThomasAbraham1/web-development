d7ebafd1db59a16628d34006665b1b2f150a5aec
ThomasAbraham1 <74496765+ThomasAbraham1@users.noreply.github.com>
2023-08-27 23:51:34 +0530

    27/8/23
    All set

---
+++ Node JS/toDoList copy/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const bodyParser = require("body-parser");
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+const todayIs = require('what-is-today');
+var todoArray = [];
+
+app.get("/", function(req, res){
+    console.log(todayIs.isToday());
+    // res.render('index', {todoArray: todoArray});
+});
+
+app.post("/", function(req,res){
+    todoArray.push(req.body.newItem);
+    console.log(todoArray);
+    res.redirect("/");
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
